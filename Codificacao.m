2e90bfb5c983010b3306bbde313ebfc04c63af2a
Kimberly Lima <kimberlyplima@hotmail.com>
2017-05-20 13:00:02 -0300

    fixing parity bit concat

--- Codificacao.m
+++ Codificacao.m
@@ -7,33 +7,25 @@
     IDarquivo = fopen(diretorio); 
     ConteudoArquivo = uint8(fread(IDarquivo, [1, inf], 'ubit1')); 
     Tam = length(ConteudoArquivo);
-
+    
+    VetorTemporario = cell(0);
+    
     for i = 1:8:Tam
-        VetorComTrc = zeros(1,8);
-        Contador = 0;
-        
-        for j = i:(i+7)
-            if (ConteudoArquivo(j) == 1)
-                Contador = Contador + 1;
-            end
-        end
-
-        if (mod(Contador, 2) ~= 0)
-            VetorComTrc(9) = 0;
+        if (i == 1)
+            VetorTemporario = ConteudoArquivo(i:i+7);
         else
-            VetorComTrc(9) = 1;
+            VetorTemporario = cat(2,VetorTemporario,ConteudoArquivo(i:i+7));
         end
-
-        VetorComTrc(1) = ConteudoArquivo(i);
-        VetorComTrc(2) = ConteudoArquivo(i+1);
-        VetorComTrc(3) = ConteudoArquivo(i+2);
-        VetorComTrc(4) = ConteudoArquivo(i+3);
-        VetorComTrc(5) = ConteudoArquivo(i+4);
-        VetorComTrc(6) = ConteudoArquivo(i+5);
-        VetorComTrc(7) = ConteudoArquivo(i+6);
-        VetorComTrc(8) = ConteudoArquivo(i+7);         
-
-        Filecodif = fopen('BitsComTRC.bin', 'a');
-        fwrite(Filecodif, VetorComTrc, 'ubit1');
-        fclose(Filecodif);
+        
+        ContadorDeUm = numel(find(ConteudoArquivo(i:i+7) == 1));
+        
+        if (mod(ContadorDeUm, 2) ~= 0)
+            VetorTemporario(length(VetorTemporario)+1) = 0;
+        else
+            VetorTemporario(length(VetorTemporario)+1) = 1;
+        end
     end
+    
+    Filecodif = fopen('BitsComTRC.bin', 'wb');
+    fwrite(Filecodif, VetorTemporario, 'ubit1');
+    fclose(Filecodif);
